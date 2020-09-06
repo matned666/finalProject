c4489a55ac38ef976a8b4efb277b4bd50c4562e9
matned <mat.niedbal@yahoo.com>
2020-09-06 11:42:10 +0200

    heroku ref

--- src/main/java/eu/mnrdesign/matned/final_project/model/User.java
+++ src/main/java/eu/mnrdesign/matned/final_project/model/User.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 import static eu.mnrdesign.matned.final_project.holder.Static.DATE_PATTERN;
 
 @Entity
+@Table(name = "USER_ENTITY")
 public class User extends BaseEntity {
 
 
